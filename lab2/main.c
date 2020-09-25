00d87980ff38e341965157b64f3599a871861dd8
Shevja <vlsdki@gmail.com>
2020-09-25 18:57:47 +0700

    modified copy function (now it's work normally)

--- lab2/main.c
+++ lab2/main.c
@@ -4,58 +4,62 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <string.h>
-int my_remove(char *arg1, char *arg2)
+
+int my_copy(const char *arg1, const char *arg2)
 {
-    int fh1, fh2;
+    int fh1, fh2;    
+    fh1 = open(arg1, O_RDONLY);
+    if (fh1 == -1)
+    {
+        printf("faile is not open\n");
+        return -1;
+    }
+
     char *namefile = malloc(sizeof(arg1));
     namefile = strrchr(arg1, '/');
-    printf("%s\n", namefile);
-    fh1 = open(arg1, O_RDONLY);
-    
+
     char *arg3 = malloc(sizeof(arg2) + sizeof(namefile));
     strcat(arg3, arg2);
     strcat(arg3, namefile);
+
     fh2 = creat(arg3, __S_IWRITE | __S_IREAD);
+    if (fh2 == -1)
+    {
+        printf("faile is not open\n");
+        return -1;
+    }
+    
+    printf("Moving\n");
     char *buf = malloc(3);
     while (read(fh1, buf, 3))
     {
         write(fh2, buf, 3);
     }
     printf("done\n");
-    
-    /*
-    struct dirent *pDirent;
-    DIR *from_dir = opendir(arg1);
-    DIR *to_dir = opendir(arg2);
-    while ((pDirent = readdir(from_dir)) != NULL) {
-        printf ("[%s]\n", pDirent->d_name);
-    }
 
-
-    closedir (from_dir);
-    closedir (to_dir);
-    */
-   close(fh1);
-   close(fh2);
-   printf("done2\n");
-   return 0;
+    close(fh1);
+    close(fh2);
+    return 0;
 }
+
 int main(int argc, char const *argv[])
 {
-    int h = my_remove("asd/test1.txt", "qwe2");
-    printf("asdasd11");
-    if (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h"))
-    {
-        printf("Авторы: Заруднев Николай, Каширин Владислав, Бесаева Далия\n");
-        printf("Доступные аргументы:\n\t-r: переместить файл\n\t-c: копировать файл\n\t-d: удалить файл\n\t-s: общий размер указанной директории или файла\n\t-f: отображение всех файлов указанной директории\n\t-p: отображение всех процессов из файловой системы procf");
-        printf("Управление файловой системой\n");
-        printf("./main --help/-h\n");
-    } else if (!strcmp(argv[1], "--remove") || !strcmp(argv[1], "-r"))
-    {
-        printf("hello\n");
-        my_remove(argv[2], argv[3]);
-    }
-    printf("asdasd");
+    //my_remove("asd/test1.txt", "qwe2");
+
+    my_copy(argv[1], argv[2]);
+
+    // if (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h"))
+    // {
+    //     printf("Авторы: Заруднев Николай, Каширин Владислав, Бесаева Далия\n");
+    //     printf("Доступные аргументы:\n\t-r: переместить файл\n\t-c: копировать файл\n\t-d: удалить файл\n\t-s: общий размер указанной директории или файла\n\t-f: отображение всех файлов указанной директории\n\t-p: отображение всех процессов из файловой системы procf");
+    //     printf("Управление файловой системой\n");
+    //     printf("./main --help/-h\n");
+    // } else if (!strcmp(argv[1], "--remove") || !strcmp(argv[1], "-r"))
+    // {
+    //     printf("hello\n");
+    //     my_remove(argv[2], argv[3]);
+    // }
+    // printf("asdasd");
     /*replace file*/
 
     /*copy file*/
