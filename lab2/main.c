79ce099c53e2c896bb52e1cecc356aa76fc45095
NikolaiZarudnev <nikolaizarudnrv@gmail.com>
2020-10-01 11:36:17 +0700

    add delete & replace

--- lab2/main.c
+++ lab2/main.c
@@ -81,47 +81,69 @@ int count_dir_size(char *dir)
     return 0;
 }
 
+int my_delete(char *dir)
+{
+    printf ("Удаление файла: ");
+   if (-1 == remove (dir))
+      printf ("Ошибка\n");
+   else 
+      printf ("Выполнено\n");
+
+   return 0;
+}
+
+int my_replace(const char *arg1, const char *arg2)
+{
+    my_copy(arg1, arg2);
+    my_delete(arg1);
+}
+
+int list_files(char *dir)
+{
+    struct dirent *pDirent;
+    DIR *pDir = opendir (dir);
+    while ((pDirent = readdir(pDir)) != NULL)
+    {
+        printf ("[%s]\n", pDirent->d_name);
+    }
+    closedir (pDir);
+}
+
 int main(int argc, char const *argv[])
 {
     list_proc(argv[1]);
 
-    // if (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h"))
-    // {
-    //     printf("Авторы: Заруднев Николай, Каширин Владислав, Бесаева Далия\n");
-    //     printf("Доступные аргументы:\n\t-r: переместить файл\n\t-c: копировать файл\n\t-d: удалить файл\n\t-s: общий размер указанной директории или файла\n\t-f: отображение всех файлов указанной директории\n\t-p: отображение всех процессов из файловой системы procf");
-    //     printf("Управление файловой системой\n");
-    //     printf("./main --help/-h\n");
-    // } else if (!strcmp(argv[1], "--remove") || !strcmp(argv[1], "-r"))
-    // {
-    //     printf("hello\n");
-    //     my_remove(argv[2], argv[3]);
-    // }
-    
-    /*replace file*/
-
-    /*copy file*/
-    //my_copy(argv[1], argv[2]);
-
-    /*delete file*/
-
-    /*total size*/
-    //count_dir_size(argv[1]);
-    /*display all files in the dir*/
-
-    /*
-
-    struct dirent *pDirent;
-
-    DIR *list_dir = opendir(arg2);
-    while ((pDirent = readdir(list_dir)) != NULL) {
-        printf ("[%s]\n", pDirent->d_name);
+    if (!strcmp(argv[1], "--help") || !strcmp(argv[1], "-h"))
+    {
+        printf("Авторы: Заруднев Николай, Каширин Владислав, Бесаева Далия\n");
+        printf("Доступные аргументы:\n\t-r: переместить файл\n\t-c: копировать файл\n\t-d: удалить файл\n\t-s: общий размер указанной директории или файла\n\t-f: отображение всех файлов указанной директории\n\t-p: отображение всех процессов из файловой системы procf");
+        printf("Управление файловой системой\n");
+        printf("./main --help/-h\n");
     }
-    
-    */
-
-    /*list all proc from /procfs */
-    //list_proc();
-
+    else if (!strcmp(argv[1], "--delete") || !strcmp(argv[1], "-d"))
+    {
+        my_delete(argv[2]);
+    }
+    else if (!strcmp(argv[1], "--replace") || !strcmp(argv[1], "-r"))
+    {
+        my_replace(argv[2], argv[3]);
+    }
+    else if (!strcmp(argv[1], "--copy") || !strcmp(argv[1], "-c"))
+    {
+        my_copy(argv[2], argv[3]);
+    }
+    else if (!strcmp(argv[1], "--size") || !strcmp(argv[1], "-s"))
+    {
+        count_dir_size(argv[2]);
+    }
+    else if (!strcmp(argv[1], "--list") || !strcmp(argv[1], "-l"))
+    {
+        list_files(argv[2]);
+    }
+    else if (!strcmp(argv[1], "--procfs") || !strcmp(argv[1], "-p"))
+    {
+        list_proc(argv[2]);
+    }
     return 0;
 }
 
