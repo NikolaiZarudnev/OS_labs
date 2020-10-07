9c09862b32eecd2dda2f9466a640db5ca67eb5b5
NikolaiZarudnev <nikolaizarudnrv@gmail.com>
2020-10-08 02:11:26 +0700

    execl_test

--- lab3/main.c
+++ lab3/main.c
@@ -7,38 +7,34 @@
 #include <ctype.h>
 #include <sys/wait.h>
 
-int lab3()
+int main(int argc)
 {
-  pid_t pid;
-  int rv;
-  switch(pid=fork()) {
-  case -1:
-          perror("fork"); /* произошла ошибка */
-          exit(1); /*выход из родительского процесса*/
-  case 0:
-          printf(" CHILD: Это процесс-потомок!\n");
-          printf(" CHILD: Мой PID -- %d\n", getpid());
-          printf(" CHILD: PID моего родителя -- %d\n",
-              getppid());
-          printf(" CHILD: Введите мой код возврата:");
-          scanf(" %d");
-          printf(" CHILD: Выход!\n");
-          exit(rv);
-  default:
-          printf("PARENT: Это процесс-родитель!\n");
-          printf("PARENT: Мой PID -- %d\n", getpid());
-          printf("PARENT: PID моего потомка %d\n",pid);
-          printf("PARENT: Я жду, пока потомок не вызовет exit...\n");
-          wait(pid);
-          printf("PARENT: Код возврата потомка:%d\n",
-                   WEXITSTATUS(rv));
-          printf("PARENT: Выход!\n");
-  }
-}
+    pid_t pid;
+    int rv;
+	char *argv[16];
+	printf("Input command: ");
+	scanf("%s", argv);
 
-int main(int argc, char const *argv[])
-{
-    lab3();
-    return 0;
+	switch(pid=fork()) {
+	case -1:
+		perror("fork"); /* произошла ошибка */
+		exit(1); /*выход из родительского процесса*/
+	case 0:
+		printf(" CHILD: Это процесс-потомок!\n");
+		printf(" CHILD: Мой PID -- %d\n", getpid());
+		printf(" CHILD: PID моего родителя -- %d\n", getppid());
+
+		execl(argv[0], " ","-s", "qwe", NULL);
+	default:
+		printf("PARENT: Это процесс-родитель!\n");
+		printf("PARENT: Мой PID -- %d\n", getpid());
+		printf("PARENT: PID моего потомка %d\n",pid);
+		printf("PARENT: Я жду, пока потомок не вызовет exit...\n");
+		wait(pid);
+		printf("PARENT: Код возврата потомка:%d\n", WEXITSTATUS(rv));
+		printf("PARENT: Выход!\n");
+	}
+	
+	
 }
 
